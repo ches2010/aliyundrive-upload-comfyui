d593f3f9ba9146252c1ace7c704bee824574b881
ches2010 <41281952+ches2010@users.noreply.github.com>
2025-09-13 11:02:32 +0800

    Create __Init__.py

---
+++ __Init__.py
@@ -0,0 +1,9 @@
+from .aliyun_upload_node import AliyunDriveUploadNode
+
+NODE_CLASS_MAPPINGS = {
+    "AliyunDriveUpload": AliyunDriveUploadNode
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "AliyunDriveUpload": "阿里云盘图片上传"
+}
