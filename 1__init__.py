fada5a7688fbbe20ed05f61a982d3788427bc36c
ches2010 <41281952+ches2010@users.noreply.github.com>
2025-09-14 13:00:54 +0800

    Update and rename 1__init__.py to aliyundrive_config.json

--- 1__init__.py
+++
@@ -1,9 +0,0 @@
-from .aliyun_upload_node import AliyunDriveUploadNode
-
-NODE_CLASS_MAPPINGS = {
-    "AliyunDriveUpload": AliyunDriveUploadNode
-}
-
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "AliyunDriveUpload": "阿里云盘图片上传"
-}
